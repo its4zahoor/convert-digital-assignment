00d14c9b97c0f08980e168682b6e0e8c9b8e7264
Abdul Zahoor <its4zahoor@gmail.com>
2024-06-08 14:00:48 +0500

    cleanup
    Signed-off-by: Abdul Zahoor <its4zahoor@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div className="card bg-blue-500">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
