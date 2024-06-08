f0c32b9f4e4184e4d0d75557b459b87868aa2deb
Abdul Zahoor <its4zahoor@gmail.com>
2024-06-08 16:01:25 +0500

    initial query to empty and only display options if query has value
    Signed-off-by: Abdul Zahoor <its4zahoor@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import DropdownOptions from "./components/DropdownOptions";
 import { useDebounce } from "./hooks/useDebounce";
 
 function App() {
-  const [query, setQuery] = useState("phone");
+  const [query, setQuery] = useState("");
 
   const debouncedQuery = useDebounce(query, 500);
 
@@ -16,9 +16,11 @@ function App() {
           onChange={(e) => setQuery(e.target.value)}
           className="leading-8 focus:outline-none focus:border-2 focus:border-blue-500 rounded-md p-2 w-full"
         />
-        <div className="absolute top-10 w-full bg-white rounded-md shadow-md">
-          <DropdownOptions query={debouncedQuery} />
-        </div>
+        {query && (
+          <div className="absolute top-10 w-full bg-white rounded-md shadow-md">
+            <DropdownOptions query={debouncedQuery} />
+          </div>
+        )}
       </div>
     </div>
   );
