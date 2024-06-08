31aa3da2b867482c03524b8086841d22bf7c5bd5
Abdul Zahoor <its4zahoor@gmail.com>
2024-06-08 14:46:08 +0500

    passes debounced query prop to options components
    Signed-off-by: Abdul Zahoor <its4zahoor@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import DropdownOptions from "./components/DropdownOptions";
+import { useDebounce } from "./hooks/useDebounce";
 
 function App() {
   const [query, setQuery] = useState("phone");
+
+  const deboncedQuery = useDebounce(query, 500);
 
   return (
     <div className="p-4 w-full">
@@ -14,7 +17,7 @@ function App() {
           className="leading-8 focus:outline-none focus:border-2 focus:border-blue-500 rounded-md p-2 w-full"
         />
         <div className="absolute top-10 w-full bg-white rounded-md shadow-md">
-          <DropdownOptions query={query} />
+          <DropdownOptions query={deboncedQuery} />
         </div>
       </div>
     </div>
