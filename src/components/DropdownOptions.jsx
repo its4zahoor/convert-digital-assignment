d7c609a2156135319d50b8f0e1d69bbfc2bb8642
Abdul Zahoor <its4zahoor@gmail.com>
2024-06-08 15:25:42 +0500

    adds selection logic to option item
    Signed-off-by: Abdul Zahoor <its4zahoor@gmail.com>

--- src/components/DropdownOptions.jsx
+++ src/components/DropdownOptions.jsx
@@ -1,18 +1,24 @@
 import { useFetchProducts } from "../hooks/useFetchProducts";
 
 const DropdownOptions = ({ query }) => {
-  const { data, loading, error } = useFetchProducts(query);
+  const { data, loading, error, reset } = useFetchProducts(query);
   if (data.length === 0 && !loading && !error) return null;
 
+  const handleSelect = (value) => () => {
+    reset();
+    console.log(value);
+  };
+
   return (
-    <div className="bg-white z-10 rounded-md">
+    <div className="bg-white z-10 rounded-md p-2">
       {loading && <div className="text-black leading-8">Loading...</div>}
       {error && <div className="text-red-300">Error: {error}</div>}
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 gap-4">
         {data.map((item) => (
           <div
             key={item.id}
             className="flex items-center hover:bg-slate-100 cursor-pointer rounded-md"
+            onClick={handleSelect(item)}
           >
             <img src={item.images[0]} alt={item.name} className="w-32 h-32" />
             <h1>{item.title}</h1>
