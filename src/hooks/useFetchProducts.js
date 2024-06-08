c71dbe943454bdc46befe3b846b32632194f4232
Abdul Zahoor <its4zahoor@gmail.com>
2024-06-08 14:54:01 +0500

    extracts data pulling, loading and error logic to a hook
    Signed-off-by: Abdul Zahoor <its4zahoor@gmail.com>

---
+++ src/hooks/useFetchProducts.js
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from "react";
+
+const API_URL = "https://dummyjson.com/products/search";
+
+export const useFetchProducts = (query) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const getData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`${API_URL}?q=${query}`);
+      const json = await res.json();
+      setData(json.products);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return { data, loading, error };
+};
